44f81e37260fd8b36399be0b0fe47b5f6f619832
Khoi Phan <khoi0611@gmail.com>
2026-05-28 21:44:58 +0700

    init backend architecture and database schema

---
+++ src/main/java/com/seal/seal_hackathon_fpt/SealHackathonFptApplication.java
@@ -0,0 +1,13 @@
+package com.seal.seal_hackathon_fpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SealHackathonFptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SealHackathonFptApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/seal/seal_hackathon_fpt/SealHackathonFptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seal.seal_hackathon_fpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SealHackathonFptApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
